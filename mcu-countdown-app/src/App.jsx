99ddf4440a818e2b660b252bd4e70e40733a96df
Mathew Dony <mathew.dony@jaxsta.com>
2022-02-28 21:15:03 +1100

    Remove unwanted files, implemented styled components

---
+++ mcu-countdown-app/src/App.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const App = () => {
+  return (
+    <AppDiv className="App">
+      <h1>Hello</h1>
+    </AppDiv>
+  );
+}
+
+const AppDiv = styled.div `
+  background-color: red;
+  text-align: center;
+`
+export default App;
