393bcca2b1b3d12040a0b0850bac69de90c88f71
Ajeeb <s3795215@student.rmit.edu.au>
2022-03-01 00:10:21 +1100

    Added API

--- mcu-countdown-app/src/components/Body.jsx
+++ mcu-countdown-app/src/components/Body.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  return (
-    <div>heloo there</div>
-  )
-}
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://www.whenisthenextmcufilm.com/api?date=2022-05-03")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setData(JSON.stringify(data));
+        });
+    };
+    fetchData();
+  }, []);
+
+  return <div>{data}</div>;
+};
 
 export default Body;
