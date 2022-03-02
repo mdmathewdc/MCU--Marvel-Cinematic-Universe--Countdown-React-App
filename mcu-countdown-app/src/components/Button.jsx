c937f302e9f07482f7ad89293291aa52a1d602d2
Mathew Dony <mathew.dony@jaxsta.com>
2022-03-02 19:14:50 +1100

    Add button loading animation

--- mcu-countdown-app/src/components/Button.jsx
+++ mcu-countdown-app/src/components/Button.jsx
@@ -1,26 +1,79 @@
 import React from "react";
 import styled from "styled-components";
+import { useRef } from "react";
 
-const Button = ({clickAction}) => {
+const Button = ({ clickAction }) => {
   const buttonText = "What's Next?";
-  return <StyledButton onClick={clickAction}>{buttonText}</StyledButton>;
+  const buttonRef = useRef(null);
+
+  const setLoader = () => {
+    buttonRef.current.classList.add("loading");
+    // Hide loader after 700 milliseconds
+    setTimeout(() => buttonRef.current.classList.remove("loading"), 700);
+  };
+
+  return (
+    <ActionButton
+      ref={buttonRef}
+      onClick={() => {
+        setLoader();
+        clickAction();
+      }}
+    >
+      {buttonText}
+    </ActionButton>
+  );
 };
 
-const StyledButton = styled.button`
+const ActionButton = styled.button`
   background-color: #eb1b20;
-  color: white;
-  padding: 10px;
-  border-radius: 50px;
+  font-family: "Poppins";
+  border: none;
   font-size: 15px;
   font-weight: 900;
-  font-family: "Poppins";
+  padding: 0.5em 1.25em;
+  color: white;
+  border-radius: 0.15em;
+  transition: 0.3s;
+  cursor: pointer;
+  position: relative;
   display: block;
-  border: 1px solid red;
   margin: 20px;
 
-  &:hover, :active {
+  &:hover {
     background-color: #fb0208;
-    cursor: pointer;
+  }
+
+  &::after {
+    content: "";
+    display: block;
+    width: 1.2em;
+    height: 1.2em;
+    position: absolute;
+    left: calc(50% - 0.75em);
+    top: calc(50% - 0.75em);
+    border: 0.15em solid transparent;
+    border-right-color: white;
+    border-radius: 50%;
+    animation: button-anim 0.7s linear infinite;
+    opacity: 0;
+  }
+
+  @keyframes button-anim {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  &.loading {
+    color: transparent;
+  }
+
+  &.loading::after {
+    opacity: 1;
   }
 `;
 
