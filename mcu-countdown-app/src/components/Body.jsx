faa3901cb90e88bf3c3222ac6cff87af97aee0fb
Mathew Dony <mathew.dony@jaxsta.com>
2022-03-02 18:10:43 +1100

    First mvp

--- mcu-countdown-app/src/components/Body.jsx
+++ mcu-countdown-app/src/components/Body.jsx
@@ -7,7 +7,6 @@ import { formatDate } from "../utils/date";
 const Body = () => {
 
   /* Current Movie/TV-Series Details */
-  const [data, setData] = useState(null);
   const [daysUntil, setDaysUntil] = useState(null);
   const [poster, setPoster] = useState(null);
   const [title, setTitle] = useState(null);
@@ -15,55 +14,39 @@ const Body = () => {
   const [releaseDate, setReleaseDate] = useState(null);
   const [mediaType, setMediaType] = useState(null);
 
-  /* Next Movie/TV-Series Details */
-  const [nextDaysUntil, setNextDaysUntil] = useState(null);
-  const [nextPoster, setNextPoster] = useState(null);
-  const [nextTitle, setNextTitle] = useState(null);
-  const [nextOverview, setNextOverview] = useState(null);
-  const [nextReleaseDate, setNextReleaseDate] = useState(null);
-  const [nextMediaType, setNextMediaType] = useState(null);
+  const fetchData = () => {
+    fetch(`https://www.whenisthenextmcufilm.com/api?date=${releaseDate}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setDaysUntil(data.days_until);
+        setPoster(data.poster_url);
+        setTitle(data.title);
+        setOverview(data.overview);
+        setReleaseDate(data.release_date);
+        setMediaType(data.type);
+      });
+  };
 
   useEffect(() => {
-    const fetchData = () => {
-      fetch("https://www.whenisthenextmcufilm.com/api?date=2022-05-03")
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-
-          // Current details
-          setData(JSON.stringify(data));
-          setDaysUntil(data.days_until);
-          setPoster(data.poster_url);
-          setTitle(data.title);
-          setOverview(data.overview);
-          setReleaseDate(formatDate(data.release_date));
-          setMediaType(data.type);
-
-          // Next details
-          setNextDaysUntil(data.following_production.days_until);
-          setNextPoster(data.following_production.poster_url);
-          setNextTitle(data.following_production.title);
-          setNextOverview(data.following_production.overview);
-          setNextReleaseDate(data.following_production.release_date);
-          setNextMediaType(data.following_production.type);
-
-        });
-    };
     fetchData();
   }, []);
+
+  const handleClick = () => {
+    fetchData();
+  };
 
   return (
     <BodyContainer>
       <h2>{title}</h2>
-      <h1><span>{daysUntil}</span> DAYS</h1>
+      <h1>
+        <span>{daysUntil}</span> DAYS
+      </h1>
       <img src={poster} alt={title} />
-      <h3>Release Date: {releaseDate}</h3>
+      <h3>Release Date: {formatDate(releaseDate)} ({mediaType})</h3>
       <p>{overview}</p>
-
-
-      {/* <p>{data}</p> */}
-      <Button />
+      <Button clickAction={handleClick} />
     </BodyContainer>
   );
 };
@@ -80,10 +63,11 @@ const BodyContainer = styled.main`
     margin: 0 10px;
     width: 90%;
     font-family: "Poppins";
+    text-align: center;
   }
 
   h1 {
-    font-family: 'Kanit', sans-serif;
+    font-family: "Kanit", sans-serif;
     margin: 0;
     text-align: center;
     line-height: 1;
@@ -102,7 +86,7 @@ const BodyContainer = styled.main`
 
   h2 {
     text-align: center;
-    font-family: 'Poppins';
+    font-family: "Poppins";
     font-weight: 100;
     font-size: 20px;
   }
--- mcu-countdown-app/src/components/Button.jsx
+++ mcu-countdown-app/src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const Button = () => {
-  return <StyledButton>What's next?</StyledButton>;
+const Button = ({clickAction}) => {
+  const buttonText = "What's Next?";
+  return <StyledButton onClick={clickAction}>{buttonText}</StyledButton>;
 };
 
 const StyledButton = styled.button`
